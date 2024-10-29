5d039eed3d9ec24615c3ca0b0dd0c88cc2c19fe0
giammin <giammin@gmail.com>
2024-10-29 17:16:40 +0100

    typos and loggings

--- src/AspNetCore.DataProtection.CustomStorage/LoggingExtensions.cs
+++ src/AspNetCore.DataProtection.CustomStorage/LoggingExtensions.cs
@@ -1,15 +1,48 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using Microsoft.Extensions.Logging;
 
 namespace AspNetCore.DataProtection.CustomStorage;
 
-internal static partial class LoggingExtensions
+/// <summary>
+/// 
+/// </summary>
+public static partial class LoggingExtensions
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="friendlyName"></param>
+    /// <param name="value"></param>
     [LoggerMessage(1, LogLevel.Debug, "Reading data with key '{FriendlyName}', value '{Value}'.", EventName = "ReadKeyFromStorage")]
     public static partial void ReadingXmlFromKey(this ILogger logger, string? friendlyName, string value);
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="friendlyName"></param>
+    /// <param name="value"></param>
+    /// <param name="storage"></param>
     [LoggerMessage(2, LogLevel.Debug, "Saving key '{FriendlyName}', value '{Value}' with '{storage}'.", EventName = "SavingKeyToStorage")]
     public static partial void LogSavingKeyToStorage(this ILogger logger, string? friendlyName, string value, string storage);
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="friendlyName"></param>
+    /// <param name="value"></param>
+    /// <param name="storage"></param>
     [LoggerMessage(3, LogLevel.Error, "Error saving key '{FriendlyName}', value '{Value}' with '{storage}'.", EventName = "ErrorSavingKeyToStorage")]
     public static partial void LogErrorSavingKeyToStorage(this ILogger logger, string? friendlyName, string value, string storage);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="config"></param>
+    /// <param name="storage"></param>
+    [LoggerMessage(4, LogLevel.Debug, "UseDapperDataProtection config:'{Config}', storage: '{storage}'.", EventName = "InitializingDapperDataProtection")]
+    public static partial void LogInitialization(this ILogger logger, string config, string storage);
 }
